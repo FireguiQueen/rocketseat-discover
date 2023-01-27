396f102fe5f39605fdd73dde0b7b4fce413459d3
Guilherme <Fireguiquenbr@gmail.com>
2023-01-27 16:06:34 -0300

    Create Ex001.js

---
+++ Trilha fundamentar/Javascript/Ex001.js
@@ -0,0 +1,22 @@
+/* FUNÇÕES
+Funções são blocos de construção fundamentais em JavaScript. 
+Uma função é um procedimento de JavaScript - um conjunto de instruções que executa uma tarefa ou calcula
+um valor. Para usar uma função, você deve defini-la em algum lugar no escopo do qual você quiser chamá-la.
+*/
+
+
+/* OBJETOS
+Um objeto é uma coleção de propriedades, e uma propriedade é uma associação entre um nome (ou chave) 
+e um valor. Um valor de propriedade pode ser uma função, que é então considerada  um método do objeto. 
+Além dos objetos que são pré-definidos no browser, você pode definir seus próprios objetos.
+Em JavaScript, um objeto é uma entidade independente, com propriedades e tipos.
+Compare-o com uma xícara, por exemplo. Uma xícara é um objeto, com propriedades. 
+Uma xícara tem uma cor, uma forma, peso, um material de composição, etc. Da mesma forma, 
+objetos em JavaScript podem ter propriedades, que definem suas características.
+*/
+
+console.log('...')
+
+// console -> função
+// .log -> objeto (o ponto final é utilizado para acessar objetos)
+// () argumentos deste objeto
