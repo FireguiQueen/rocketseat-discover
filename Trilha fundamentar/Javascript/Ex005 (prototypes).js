276298f56ee7e4c098c31bf60397c8a3fdabd514
Guilherme <Fireguiquenbr@gmail.com>
2023-01-31 23:53:14 -0300

    Update Ex005 (prototypes).js

---
+++ Trilha fundamentar/Javascript/Ex005 (prototypes).js
@@ -0,0 +1,6 @@
+// Prototypes
+
+// São basicamente funcionalidades que já foram criadas pela própria linguagem
+// Isto significa que não precisamos criar necessiariamente uma função para cada coisa que queremos fazer.. Exemplo.:
+// --> Você deseja saber quantas letras existem em determinada string
+         // |-> Não será necessário criar uma função do 0 para isto, pois já existe
