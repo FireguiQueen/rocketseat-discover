546d0cb3df1a77bcb4c328244e4d1ea08d0cd66a
Guilherme <Fireguiquenbr@gmail.com>
2023-01-31 23:56:02 -0300

    Update Ex005 (prototypes).js

--- Trilha fundamentar/Javascript/Ex005 (prototypes).js
+++ Trilha fundamentar/Javascript/Ex005 (prototypes).js
@@ -1,6 +1,11 @@
 // Prototypes
 
 // São basicamente funcionalidades que já foram criadas pela própria linguagem
+// Você pode acessar esses protótipos usando uma string e colocando um ponto final (.), você verá várias funções, isto são prototypes
 // Isto significa que não precisamos criar necessiariamente uma função para cada coisa que queremos fazer.. Exemplo.:
 // --> Você deseja saber quantas letras existem em determinada string
-         // |-> Não será necessário criar uma função do 0 para isto, pois já existe
+         // |-> Não será necessário criar uma função do 0 para isto, pois já existe um método para isto
+
+
+var frase = "Olá, isto é uma frase".replace('Olá', 'oi')
+console.log(frase)
