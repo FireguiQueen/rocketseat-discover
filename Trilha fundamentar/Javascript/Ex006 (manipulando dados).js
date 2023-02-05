b94aa41a91a156bac37fefdca02ccc3a1c2b469b
Guilherme <Fireguiquenbr@gmail.com>
2023-02-05 17:04:01 -0300

    Update Ex006 (manipulando dados).js

--- Trilha fundamentar/Javascript/Ex006 (manipulando dados).js
+++ Trilha fundamentar/Javascript/Ex006 (manipulando dados).js
@@ -1,7 +1,8 @@
-/* Type conversion  vs  type coersion 
+// CONVERTENDO TIPOS DE DADOS
 
+/* Type conversion  vs  type coersion 
 // -> Conversão de um tipo de dado para outro tipo
-// -> Pode ser feito pelo próprio JS ou por nós
+// -> Pode ser feito pelo próprio JS ou por nós usando métodos
 */
 
 // Convertando uma STRING para NUMBER
@@ -34,27 +35,26 @@ console.log(55 + Number('4'))
 
 
 
-// Contando quantos caracteres há em determinada frase/valor numérico
+// CONTANDO QUANTOS CARACTERES HÁ EM DETERMINADA FRASE/VALOR NUMÉRICO
 
 // Funciona perfeitamente
 let nome = "Guilherme".length
 console.log(nome) // 9
 
 
-
-// O resultado acima será "undefined" pois o LENGTH não funciona para tipos de dados "NUMBERS"
+// O resultado abaixo será "undefined" pois o LENGTH não funciona para tipo de dado "NUMBER"
 let digitos = 844
 console.log(digitos.length)
 
 // Então uma conversão forçada será necessária
-let digitos_2 = String(844)
-console.log(digitos_2.length) // 3 (pois há 3 caracteres na string)
+let digitos_2 = 844
+console.log(String(digitos_2).length) // 3 (pois há 3 caracteres na string)
 
 // Tome cuidado caso haja um valor quebrado 
 let digitos_3 = String(844.55)
-console.log(digitos_3.length) // 6 (pois há 6 caracteres)
+console.log(digitos_3.length) // 6 (pois há 6 caracteres - a vírgula também conta como caracter)
 
-// Podemos resolver isto assim
+// Podemos resolver isto assim.:
 let digitos_4 = String(424.22)
 console.log(digitos_4.replace('.','').length)
 
@@ -65,7 +65,7 @@ console.log(digitos_4.replace('.','').length)
 
 
 
-// Adicionando casas decimais
+// ADICIONANDO CASAS DECIMAIS
 let wallet = 54 
 console.log(wallet.toFixed(2))
 
@@ -73,3 +73,33 @@ console.log(wallet.toFixed(2))
 // Também podemos utilizar do replace
 let green_wallet = 78
 console.log(green_wallet.toFixed(2).replace('.',','))
+
+
+
+
+
+// STRINGS EM MAIÚSCULAS
+let grito = 'olá, está me ouvindo?'
+console.log(grito.toUpperCase())
+
+// STRINGS EM MINÚSCULAS
+let comprimentar = 'OLÁ, TUDO BEM? COMO VAI?'
+console.log(comprimentar.toLowerCase())
+
+
+
+
+
+// CRIANDO ARRAYS UTILIZANDO STRINGS
+let phrase = "Esta pequena frase será dividida e irá entrar num Array"
+
+let list_phrase = phrase.split('d') // Aqui dentro passamos como a string será dividida no texto
+                                    // Ou seja, a cada caracter "d" encontrado na string, a array será dividida 
+                                    // e o "d" será cortado
+console.log(list_phrase)
+
+// TAMBÉM PODEMOS CRIAR STRINGS UTILIZANDO ARRAYS (E AINDA SEPARANDO OS ITENS)
+// O "join" nos permite separar itens da nosta lista quando vermos em formato string
+let fruits = ['mango', 'avocado', 'watermelon', 'banana']
+let list_fruits = fruits.join(`, `)
+console.log(list_fruits)
