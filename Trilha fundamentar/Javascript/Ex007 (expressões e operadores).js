800c0b3755304a0ea15f6142933df1d131e470dc
Guilherme <Fireguiquenbr@gmail.com>
2023-02-06 19:18:36 -0300

    Update Ex007 (expressões e operadores).js

---
+++ Trilha fundamentar/Javascript/Ex007 (expressões e operadores).js
@@ -0,0 +1,41 @@
+/* 
+Operadores.:
+Em JavaScript, existem três tipos de operadores: binários, unários e ternários.
+
+    Unary
+    -> trabalha com um operando
+
+    Binary
+    -> trabalha com dois operandos 
+
+    Ternary
+    -> trabalha com 3 operandos
+*/
+
+
+//                                  UNARY
+
+// Operadores: '++', '--'
+let saldo = 42
+console.log(++saldo) // adiciona +1 a variável "saldo"
+
+let saldo2 = 20
+console.log(--saldo2) // remove -1 a variável "saldo2"
+
+
+
+
+//                                  BINARY
+
+// Operadores: '>' ,  '<' , '>=' ,  '<='.. E diversos outros
+let Grupo_A = 343
+let Grupo_B = 41
+
+// Subtração
+let diferenca = Grupo_A - Grupo_B
+console.log(diferenca)
+
+// Maior
+let MaiorGrupo = Grupo_A > Grupo_B  // irá retornar "true"
+console.log(MaiorGrupo)
+
