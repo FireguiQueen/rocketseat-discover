20721ac96f3c2bff7e5c0e5b9f558876c0a892af
Guilherme <Fireguiquenbr@gmail.com>
2023-02-15 20:59:27 -0300

    update

--- Trilha fundamentar/Javascript/Ex001 (conceitos básicos).js
+++ Trilha fundamentar/Javascript/Ex001 (conceitos básicos).js
@@ -5,31 +5,40 @@ Uma função é um procedimento de JavaScript - um conjunto de instruções que 
 um valor. Para usar uma função, você deve defini-la em algum lugar no escopo do qual você quiser chamá-la.
 */
 
+function Diga_Oi(){     // Declarando a função
+   console.log('Oi');   // Declarando o corpo da função 
+}                       // Fechando a função
+Diga_Oi();              // Invocando a função
+
+
+
+
+
 
 /*                                   OBJETOS
 
-Um objeto é uma coleção de propriedades, e uma propriedade é uma associação entre um nome (ou chave) 
-e um valor. Um valor de uma propriedade pode ser uma função, que é então considerada um "método" do objeto. 
-Além dos objetos que são pré-definidos no browser (protypes), você pode definir seus próprios objetos.
-Em JavaScript, um objeto é uma entidade independente, com propriedades e tipos.
+Um objeto é uma coleção de propriedades, e uma propriedade é uma associação entre um nome 
+e um valor. Um valor de uma propriedade pode ser uma função, que é então considerada um "método". 
 Compare-o com uma xícara, por exemplo. Uma xícara é um objeto, com propriedades. 
 Uma xícara tem uma cor, uma forma, peso, um material de composição, etc. Da mesma forma, 
 objetos em JavaScript podem ter propriedades, que definem suas características.
 */
 
-// EXEMPLO
+
 console.log('...')
 // console -> objeto
-// .log -> metódo (o ponto final é utilizado para acessar propriedades)
-// () argumentos deste metódo
+// .log    -> metódo (o ponto final é utilizado para acessar propriedades)
+// ()      -> argumentos deste metódo
+// Podemos dizer que é uma função do objeto ou simplesmente "método"
+
 
 // Criando um objeto
-const pessoa = {      // Objeto em si
-   nome: 'Paulo',     // nome:   property name    ,  "Paulo": property value
-   idade: 19          // idade:  property name    ,       19: property value
+const pessoa = {      // Declarando o objeto
+   nome: 'Paulo',     // nome:   property name    |    "Paulo": property value
+   idade: 19          // idade:  property name    |         19: property value
 }
 console.log(pessoa.nome);
-             //|-> Primeiro chamamos o Objeto em si "pessoa", e depois uma de suas propriedades.
+             //|-> Primeiro chamamos o Objeto (pessoa), e depois uma de suas propriedades.
 
 
 
@@ -38,11 +47,10 @@ console.log(pessoa.nome);
 /* ------------------------------------------------------------------------------------------------- */
 
 
-
-
 //                               Comentários em JS 
 
 // Utilize-se " // " para fazer um comentário de uma única linha
+
 
 /* 
 Utiliza " /* " para fazer um comentário de bloco 
