67c170b95b306e44fa5c1f5dfc90aae4560441e4
Guilherme <Fireguiquenbr@gmail.com>
2023-02-20 17:47:56 -0300

    Update Ex009 (estruturas de repetição).js

--- Trilha fundamentar/Javascript/Ex009 (estruturas de repetição).js
+++ Trilha fundamentar/Javascript/Ex009 (estruturas de repetição).js
@@ -6,9 +6,53 @@
 // O que foi dito no código abaixo.:
 /*
     I é igual a 0 (zero)
-    Enquanto I for menor ou igual a zero, excecute o bloco de códigos abaixo
+    Enquanto I for menor que 10, excecute o bloco de códigos abaixo
     E no final, somente I + um
 */
-for(let i = 0; i <= 10; i++){
-    console.log('oi1')
+for(let i = 0; i < 10; i++){
+    console.log(`${i}`)
+}
+
+console.log('-------------')
+
+// Também podemos controlar melhoar nossa estrutura de repetição utilizando o "if" e "break"
+
+/*
+    Foi dito que, caso a variável I seja menor ou igual a zero   OU     I seja maior que 100
+    O código irá quebrar, ou seja, irá parar de executar
+*/
+for(let i = 1; i <= 100; i++){
+    if(i <= 0 || i > 100){
+        break;
+    }
+    console.log(i)
+}
+
+/*
+    O "continue" serve para ignorar determinada parte do loop
+    Porém continuar exutando o mesmo
+*/
+
+for(let i = 100; i <= 1000; i+= 100){
+    if(i == 500){
+        continue;
+    }
+    console.log(i)
+}
+
+
+
+/* 
+    O "continue" pode ser muito útil por exemplo para puxar
+    apenas números pares
+
+    Foi dito o seguinte
+    SE o resta da divisão entre I dividido por dois for DIFERENTE de 0 (resulta num número impar)
+    ele irá ignorar esses números no loop
+*/ 
+for(let i = 1; i < 10; i++){
+    if(i % 2 != 0){
+        continue;
+    }
+    console.log(i)
 }
