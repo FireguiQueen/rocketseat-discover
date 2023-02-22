5e31caf9a8138599824dc2406abbd4997f56fecc
Guilherme <Fireguiquenbr@gmail.com>
2023-02-22 16:57:24 -0300

    Update tests.js

--- Trilha fundamentar/Javascript/tests.js
+++ Trilha fundamentar/Javascript/tests.js
@@ -757,39 +757,39 @@ const booksByCategory = [
    /* categoria*/{
        category: "Riqueza",
        books: [
-           {
+         {
                title: "Os segredos da mente milionária",
                author: "T. Harv Eker",
-           },
-           {
+            },
+            {
                title: "O homem mais rico da Babilônia",
                author: "George S. Clason",
-           },
-           {
+            },
+            {
                title: "Pai rico, pai pobre",
                author: "Robert T. Kiyosaki e Sharon L. Lechter",
-           },
-       ],
-   },
-
-   {
-       category: "Inteligência Emocional",
-       books: [
-           {
+            },
+         ],
+      },
+      
+      {
+         category: "Inteligência Emocional",
+         books: [
+            {
                title: "Você é Insubstituível",
                author: "Augusto Cury",
            },
            {
                title: "Ansiedade – Como enfrentar o mal do século",
                author: "Augusto Cury",
-           },
-           {
+            },
+            {
                title: "Os 7 hábitos das pessoas altamente eficazes",
                author: "Stephen R. Covey",
-           },
-       ],
-   },
-];
+            },
+         ],
+      },
+   ];
 /* 
     Baseado no Array de Livros por Categoria acima, faça os seguintes desafios
         • Contar o número de categorias
@@ -807,7 +807,7 @@ console.log(countCategories())
 
 function countBooks(){
    for(let category of booksByCategory){
-      for(let books of booksByCategory.book){
+      for(let books of category.books){
 
       }
    }
