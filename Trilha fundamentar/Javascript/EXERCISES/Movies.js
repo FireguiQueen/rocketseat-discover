79e7836855751258c758c9c0b104781ce3da72ad
Guilherme <Fireguiquenbr@gmail.com>
2023-02-25 23:27:13 -0300

    Update Movies.js

---
+++ Trilha fundamentar/Javascript/EXERCISES/Movies.js
@@ -0,0 +1,28 @@
+const movies = [
+    { title: "The Shawshank Redemption", director: "Frank Darabont", year: 1994 },
+ 
+    { title: "The Godfather", director: "Francis Ford Coppola", year: 1972 },
+ 
+    { title: "The Dark Knight", director: "Christopher Nolan", year: 2008 },
+ 
+    { title: "12 Angry Men", director: "Sidney Lumet", year: 1957 },
+ 
+    { title: "Schindler's List", director: "Steven Spielberg", year: 1993 },
+ 
+    { title: "The Lord of the Rings: The Return of the King", director: "Peter Jackson", year: 2003 },
+ 
+    { title: "Pulp Fiction", director: "Quentin Tarantino", year: 1994 },
+ 
+    { title: "The Good, the Bad and the Ugly", director: "Sergio Leone", year: 1966 },
+ 
+    { title: "Forrest Gump", director: "Robert Zemeckis", year: 1994 },
+ 
+    { title: "Inception", director: "Christopher Nolan", year: 2010 }
+  ];
+ 
+
+// Conte o número de filmes
+
+
+
+// Liste todos os filmes em ordem alfabética. [concluído]
