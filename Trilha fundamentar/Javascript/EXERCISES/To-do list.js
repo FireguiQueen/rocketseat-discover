647aef4abe9f03a85e205390b9db909cf3b9c82b
Guilherme <Fireguiquenbr@gmail.com>
2023-02-27 20:26:57 -0300

    update

---
+++ Trilha fundamentar/Javascript/EXERCISES/To-do list.js
@@ -0,0 +1,15 @@
+/*
+  Crie um programa que gerencie uma lista de tarefas (to-do list).
+  Cada tarefa é composta por um título e uma descrição.
+  O programa deve ser capaz de:
+  - Adicionar uma nova tarefa
+  - Remover uma tarefa existente
+  - Listar todas as tarefas
+  - Marcar uma tarefa como concluída
+  - Desmarcar uma tarefa concluída
+  - Listar todas as tarefas concluídas
+*/
+
+function creatList(title, description, complete = 'false'){
+    let to_do = [{}]
+}
