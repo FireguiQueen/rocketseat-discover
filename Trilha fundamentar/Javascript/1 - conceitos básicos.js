40ed1984869d4b0c0471c343fee6e997841edac9
Guilherme <Fireguiquenbr@gmail.com>
2023-03-22 19:11:50 -0300

    Update 1 - conceitos básicos.js

--- Trilha fundamentar/Javascript/1 - conceitos básicos.js
+++ Trilha fundamentar/Javascript/1 - conceitos básicos.js
@@ -1,7 +1,7 @@
 /*                                   FUNÇÕES
 
-Funções são blocos de construção fundamentais em JavaScript. 
-Uma função é um procedimento de JavaScript - um conjunto de instruções que executa uma tarefa ou calcula
+Funções são blocos de construção e são fundamentais em JavaScript ou em qualquer outra linguagem. 
+Uma função é um conjunto de instruções que executa uma tarefa ou calcula
 um valor. Para usar uma função, você deve defini-la em algum lugar no escopo do qual você quiser chamá-la.
 */
 
